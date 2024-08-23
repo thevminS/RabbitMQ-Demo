e5b8adb231ff6c0a0546209414f65928f13ea3fa
Thevmin Sanhinda <owtsanhida@gmail.com>
2024-08-23 16:42:45 +0530

    Created two ms for rabbitmq request-reply pattern

---
+++ pcw/src/test/java/com/rabitmq/pcw/PcwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabitmq.pcw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
